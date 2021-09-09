d809dd159e46af6a7b90d306b71cf9bd8592db68
phc <phc2311128348@126.com>
2021-09-09 07:21:41 +0000

    修改了基础页面

--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>微博</title>
-</head>
-<body>
-<h1>主页</h1>
-</body>
-</html>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>@yield('title','微博') - Laravel_微博教程</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
