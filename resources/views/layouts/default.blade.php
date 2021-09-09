f41d359a5c55881ada1fa807687cf40fc947ecdd
phc <phc2311128348@126.com>
2021-09-09 09:48:09 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,22 @@
 <html>
 <head>
     <title>@yield('title','微博') - Laravel_微博教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Weibo</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link" href="/help">帮助</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">登录</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 @yield('content')
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-    <h1>主页</h1>
+    <div claSS="jumbotron">
+        <h1>主页</h1>
+        <p class="lead">你现在看到的是</p>
+        <p>一切从这里开始</p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#"
+               role="button">现在注册</a>
+        </p>
+    </div>
 @stop
