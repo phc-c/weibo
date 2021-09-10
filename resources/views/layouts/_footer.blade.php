8a0464c82839d2d720accdc35bbcc537433dad91
phc <phc2311128348@126.com>
2021-09-10 01:41:19 +0000

    用户注册页面

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="footer">
    <div class="float-right">
+       <span>不断学习，锐意进取 </span>
        <a href="{{route('about')}}">关于</a>
    </div>
 </footer>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,7 +6,8 @@
         <p class="lead">你现在看到的是</p>
         <p>一切从这里开始</p>
         <p>
-            <a class="btn btn-lg btn-success" href="#"
+            <a class="btn btn-lg btn-success"
+               href="{{route('signup')}}"
                role="button">现在注册</a>
         </p>
     </div>
