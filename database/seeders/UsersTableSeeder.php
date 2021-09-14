8f629ec54bb5ed370606df8b2f5560207ac089cc
phc <phc2311128348@126.com>
2021-09-14 03:02:15 +0000

    设置管理员(失败)

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = '黄依依';
         $user->email = 'huangyiyi.sailvan.com';
+        $user->is_admin=true;
         $user->save();
     }
 }
